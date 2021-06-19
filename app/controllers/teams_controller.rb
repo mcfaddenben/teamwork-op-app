5e2bbec532013195e69f388ba2a8b5dc42f603ff
Ben McFadden <mcfaddenben64@gmail.com>
2021-06-19 13:23:52 -0400

    adds partial CRUD actions for models, saving for later

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,24 @@
 class TeamsController < ApplicationController
+    def index
+     teams = Team.where(private: true)
+        if params[:search_term]
+            teams = teams.where("name ilike ?", "%#{params[:search_term]}%")
+        end
+            
+
+     render json: teams
+    end
+    def create
+        team = Team.new(
+            name:  params[:name],
+            about:  params[:about],
+            user_id: params[:user_id],
+            private:  params[:private]
+        )
+        if team.save
+            render json: team
+        else
+            render json: {errors: team.errors.full_mesages} status: :bad_request
+        end
+    end
 end
